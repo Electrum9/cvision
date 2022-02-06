e6f58cdfdd26c039295e2f7cd6625e794c289ef1
Electrum9 <vikram.bhagavatula@gmail.com>
2022-02-05 16:13:38 -0800

    Finished hw 5

---
+++ hw5/hw5.md
@@ -0,0 +1,66 @@
+---
+title: "Homework 5"
+author: 
+    - Vikram Bhagavatula
+date: Feb 05, 2022
+documentclass: article
+---
+\newpage
+
+# Code
+
+```{.python include="./src/hw5.py"}
+```
+
+\newpage
+# Responses
+
+@. The fundamental matrix computed between the left and right image:
+
+$$
+\begin{bmatrix}
+ 9.45781783e-06 &  2.42273731e-05 & -1.10184261e-02 \\
+-1.76093034e-05 &  8.61231351e-06 & -6.28255876e-03 \\
+ 7.12348483e-03 & -3.93744237e-04 &  1.00000000e+00 \\
+\end{bmatrix}
+$$
+
+@. Example outputs:
+
+![Checkerboard at (410, 199)](./src/checkerboard.png)
+
+![Android at (191, 312)](./src/android.png)
+
+\newpage
+@. Homography
+
+- Matrix:
+
+$$
+\begin{bmatrix}
+0.448719   & 0.0377775   & 104.207  \\
+0.0416049  & 0.835691    & -10.2412 \\
+-0.00100847 & 0.000526905 &   1      \\
+\end{bmatrix}
+$$
+
+- Points:
+
+Left Points                         Right Points
+---------------------------         -----------------------------
+(147.09922457, 63.96668707)         (195.6452913, 55.25329048)
+(182.26400367, 63.96668707)         (220.85190287, 59.92118151)
+(413.4802061, 267.48673605)         (415.03616978, 320.07830834)
+(438.68681767, 265.3083869)         (445.22186512, 327.85812672)
+(220.85190287, 137.0969799)         (245.12493623, 133.67385981)
+(251.34879094, 135.22982349)        (272.19870421, 139.27532905)
+(353.42000816, 204.31461075)        (358.39909193, 231.077186)
+(380.49377615, 201.51387614)        (392.63029283, 242.28012448)
+
+- Examples:
+
+![Original Image](./src/left.jpg)
+
+![Target Image](./src/right.jpg)
+
+![Generated Image](./src/better_homography_left_right.png)
--- hw5/src/hw5.py
+++ hw5/src/hw5.py
@@ -14,6 +14,37 @@ from skimage import io
 from matplotlib import pyplot as plt
 from matplotlib import cm
 
+def homography(p1, p2):
+    def getSysEqns(a, b):
+        """
+        Returns back pair of system of equations (rows of a matrix) 
+        corresponding to the point pair.
+        """
+        (xa, ya) = a
+        (xb, yb) = b
+
+        row1 = [xa, ya, 1, 0, 0, 0, -xb*xa, -ya*xb]
+        row2 = [0, 0, 0, xa, ya, 1, -xa*yb, -ya*yb]
+
+        return [row1, row2]
+
+    concatMap = lambda f, x: list(chain.from_iterable(map(f, x)))
+    # Maps over x, producing a bunch of iterables (lists) and 
+    # then chain will yield the values from each one in the given order
+    point_pairs = zip(p1, p2)
+
+    A = np.array(concatMap(lambda p: getSysEqns(*p), point_pairs)) 
+    # Entire system of eqns assembled from pairs of systems of 
+    # equations for each point pair
+    b = np.array(concatMap(list, p2))
+    # p2 consists of points (x', y') and the like, 
+    # so we convert all pairs to lists, concatenate them, and form an array
+    h = np.append(np.linalg.lstsq(A, b)[0], 1)
+    H = np.reshape(h, (3, 3))
+    print("H matrix")
+    print(tabulate(H, tablefmt="latex"))
+    return H
+
 def drawEpipolarLine(point, right_image, fmat):
     hc_point = np.append(np.array(point), 1) # same point in homogenous coordinates
 
@@ -43,12 +74,6 @@ def requestPoints(images, npoints, timeout=60):
     requests it from each image, and then yields the points from the image, one by one. The points are
     saved between runs, so if the same image pops up again in the collection on a separate run, the saved
     points are used instead of requesting them again.
-    Example usage:
-
-    >>> pt_collection = list(requestPoints([img1, img2], 8))
-    >>> pts_img1 = pt_collection[0]
-    >>> pts_img2 = pt_collection[2]
-    
     """
     for img in images:
         plt.imshow(img)
@@ -58,7 +83,20 @@ def epiline(file1, file2):
     img1 = io.imread(file1)
     img2 = io.imread(file2)
 
-    pts1, pts2 = tuple(requestPoints((img1, img2), 8))[:2]
+    filename1 = file1.split('.')[0] # split around file extension, get name
+    filename2 = file2.split('.')[0] # split around file extension, get name
+
+    try:
+        zipped_pts = np.load(f"{filename1}_epi_{filename2}.npy") # zipped_points is an array with two rows consisting of tuples (requires enabling pickling to save them)
+        pts1 = zipped_pts[0]
+        pts2 = zipped_pts[1]
+    except Exception as e:
+        print(e)
+        # (p1, p2) = requestPoints((img1, img2), 8) # presents both given image and blank image
+        pts1, pts2 = tuple(requestPoints((img1, img2), 8))[:2]
+        savename = f"{filename1}_epi_{filename2}.npy"
+        np.save(savename, np.array([pts1, pts2]), allow_pickle=True)
+
 
     fmat = cv2.findFundamentalMat(np.array(pts1), np.array(pts2), cv2.FM_8POINT)[0] # Uses 8-point algorithm to compute fundamental matrix
     print(fmat)
@@ -68,15 +106,70 @@ def epiline(file1, file2):
     print(pt)
     img2_eline = drawEpipolarLine(pt, img2, fmat)
 
-    img1 = cv2.circle(img1, pt, radius=0, color=(0,255,0), thickness=2) # image annotated with point chosen
+    img1 = cv2.circle(img1, pt, radius=0, color=(0,255,0), thickness=5) # image annotated with point chosen
 
     res = np.concatenate((img1, img2_eline), 1)
-    plt.imsave("result.jpg", res)
+    # plt.imsave("result.jpg", res)
+    return {"Result": res}
     # plt.imshow(np.concatenate((img1, res), 1))
+
+def convertPerspective(file1, file2, npoints="8"):
+    """
+    Performs "convert perspective" routine for part 2 of the homework.
+    """
+    npoints = int(npoints)
+    img1 = io.imread(file1)
+    img2 = io.imread(file2)
+
+    filename1 = file1.split('.')[0] # split around file extension, get name
+    filename2 = file2.split('.')[0] # split around file extension, get name
+
+    try:
+        zipped_pts = np.load(f"{filename1}_to_{filename2}.npy") # zipped_points is an array with two rows consisting of tuples (requires enabling pickling to save them)
+        pl = zipped_pts[0]
+        pr = zipped_pts[1]
+    except Exception as e:
+        print(e)
+        (pl, pr) = tuple(requestPoints((img1, img2), npoints))[:2] # presents both given image and blank image
+        print(f"{pl=}")
+        np.save(f"{filename1}_to_{filename2}.npy", np.array([pl, pr]), allow_pickle=True)
+
+    print("Points")
+    # print(tabulate(np.array([pl, pr]), tablefmt="latex"))
+    print(np.array([pl, pr]))
+    H21 = homography(pl, pr)
+    res = cv2.warpPerspective(img1, H21, (img1.shape[1], img1.shape[0]))
+
+    return {"Source": img1,
+            "Target": img2,
+            "Result": res}
 
 def main():
     f1, f2 = sys.argv[1:3]
     res = epiline(f1, f2)
+
+def displayImages(labeled_images):
+    for (label, img) in labeled_images.items():
+        plt.figure(label)
+        plt.imshow(img)
+    plt.show()
+
+def main():
+    """
+    Usage: ./hw3.py [routine alias] [arg1] [arg2] [number of points=8]
+    The program will save the points you select into *.npy files, so on subsequent runs it will load from these files and will not request user input.
+    To manually input the points again, you need to delete the *.npy files (use `make clean` to remove all *.npy files).
+    """
+
+    routine = { "ep" : (epiline, 2),
+                "cp" : (convertPerspective, 2),
+              }
+    # Dictionary that maps routine alias to (function, number of args)
+
+    (curr_routine, args) = routine[sys.argv[1]]
+    params = tuple(sys.argv[2:2+args])
+    labeled_images = curr_routine(*params)
+    displayImages(labeled_images)
 
 if __name__=="__main__":
     main()
